bf3cdcfb7b21edc43af300118b9f8a1a35755ddc
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-03-04 08:44:43 +0100

    WIP.

--- cloud_colocations/data/__init__.py
+++ cloud_colocations/data/__init__.py
@@ -9,6 +9,8 @@ import socket
 from tempfile import NamedTemporaryFile
 import subprocess
 from netCDF4 import Dataset
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 
 
 y_mean = np.array([227.60539246, 227.60655212, 227.59770203, 227.59559631,
@@ -133,3 +135,63 @@ class GpmColocations(torch.utils.data.Dataset):
             raise e
 
         return torch.tensor(x[:, 2:-1, 2:-1]).float(), torch.tensor(y[2:-1, 2:-1]).float()
+
+
+class GpmData:
+    def __init__(self, filename, n = -1):
+        self.file = Dataset(filename)
+
+        x = self.file["x"][:n, :, :, :]
+
+        valid = []
+        x_normed = np.zeros(x.shape, dtype=np.float32)
+
+        for i in range(x.shape[-1]):
+
+            valid = np.logical_and(x[:, :, :, i] > 0.0,
+                                   x[:, :, :, i] < 500.0)
+            x_min = x[valid, i].min()
+            x_max = x[valid, i].max()
+            x_normed[:, :, :, i] = -0.9 + 1.9 * (x[:, :, :, i] - x_min) / (x_max - x_min)
+            x_normed[~valid, i] = -1.0
+            print(x_min, x[:, :, :, i].min(), x_max, x[:, :, :, i].max(), x_normed[:, :, i].min())
+
+        self.x = np.transpose(x_normed, (0, 3, 1, 2))
+
+        self.y = self.file["y"][:n, :, :]
+        valid = self.y >= 0.0
+        self.y[~valid] = -1.0
+
+        n = self.y.shape[1] // 2
+        self.y[:, :, :n - 10] = -1
+        self.y[:, :, (n + 10) :] = -1
+
+    def __len__(self):
+        return self.x.shape[0]
+
+    def __getitem__(self, index):
+        x = torch.tensor(self.x[index, :, 3:-2, 3:-2])
+        y = torch.tensor(self.y[index, 3:-2, 3:-2])
+        y[:10, :] = -1
+        y[-10:, :] = -1
+        return (x, y)
+
+    def plot_colocation(self):
+
+        ind = np.random.randint(self.x.shape[0])
+
+        f = plt.figure(figsize = (16, 4))
+        gs = GridSpec(1, 4)
+
+        for i, j in enumerate([5, 9, 12]):
+            ax = plt.subplot(gs[i])
+            ax.pcolormesh(self.x[ind, :, :, j])
+
+        ax = plt.subplot(gs[-1])
+        ax.pcolormesh(self.y[ind, :, :])
+        return f
+
+
+
+
+
